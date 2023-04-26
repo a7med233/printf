c2d328dbf7d891331232d50c28b868efa13b7c72
hanaharidy <125611027+hanaharidy@users.noreply.github.com>
2023-04-26 04:30:29 +0200

    Update specifier.c

--- specifier.c
+++ specifier.c
@@ -94,47 +94,4 @@ int get_flag(char *s, params_t *params)
  *
  * Return: if modifier was valid
  */
-int get_modifier(char *s, params_t *params)
-{
-	int i = 0;
-
-	switch (*s)
-	{
-	case 'h':
-		i = params->h_modifier = 1;
-		break;
-	case 'l':
-		i = params->l_modifier = 1;
-		break;
-	}
-	return (i);
-}
-
-/**
- * get_width - gets the width from the format string
- * @s: the format string
- * @params: the parameters struct
- * @ap: the argument pointer
- *
- * Return: new pointer
- */
-char *get_width(char *s, params_t *params, va_list ap)
-{
-	int d = 0;
-
-	if (*s == '*')
-	{
-		d = va_arg(ap, int);
-		s++;
-	}
-	else
-	{
-		while (_isdigit(*s))
-			d = d * 10 + (*s++ - '0');
-	}
-	params->width = d;
-	return (s);
-}
-
-
 
